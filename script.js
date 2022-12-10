38905dbaf44fed792363352366962ec801a99a2a
jack <Ciao@lazych.me>
2022-12-10 15:04:18 +0100

    changes

--- script.js
+++ script.js
@@ -7,11 +7,18 @@
 async function start() {
     const response = await fetch(`https://dog.ceo/api/breeds/list/all`)
     const data = await response.json()
-    createBreedList(data)
+    createBreedList(data.message)
 }
 
 start()
 
-function createBreedList(data) {
-
+function createBreedList(dogsData) {
+    document.getElementById("breed").innerHTML = `
+        <select>
+            <option>choose a dog breed</option>
+            ${Object.keys(dogsData).map(item => {
+        return `<option>${item}</option>`
+    }).join('')}
+        </select>
+     `
 }

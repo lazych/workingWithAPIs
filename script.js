dfa7f30a095fe580581b57bb7eac2fd408c9c34e
jack <Ciao@lazych.me>
2022-12-12 23:46:49 +0100

    testing dogs API

--- script.js
+++ script.js
@@ -1,79 +1,103 @@
-// fetch(`https://dog.ceo/api/breeds/list/all`).then(function (response) {
-//     return response.json()
-// }).then(function (data) {
-//     console.log(data)
-// })
+//assinig variables
+let timer;
+let deleteFirstPhotoDelay;
 
-
-let timer
-let deleteFirstPhotoDelay
-//fetching the breed list
-async function start() {  
-    try {
-        const response = await fetch("https://dog.ceo/api/breeds/list/all")
-        const data = await response.json()
-        createBreedList(data.message)        
-    } catch (e) {
-        console.log(`wa sf ghayrha!`)
-    }
+//fetching the breed list function
+async function start() {
+  // we use try/catch in case an error is happened. internet down or smthg.
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await response.json();
+    //calling createBreedList function and passing the fectched data as an argument
+    createBreedList(data.message);
+  } catch (e) {
+    // e means error, it contain error information in case
+    console.log(e);
+  }
 }
 
-start()
+//fetching the breed list
+start();
 
-//creating the select element
+//creating breed list selector
+// Object.keys(object) ==> The Object.keys() method returns an array.
+// this.value == african, agita, beagle..
 function createBreedList(data) {
-    document.getElementById('breed').innerHTML = `
+  document.getElementById("breed").innerHTML = `
     <select onchange="loadByBreed(this.value)"> 
         <option>choose a dog breed</option>
-        ${Object.keys(data).map(item => {
-            return `<option>${item}</option>`
+        ${Object.keys(data).map((item) => {
+          return `<option>${item}</option>`;
         })}
     </select>
-    `
+    `;
 }
 
 async function loadByBreed(breed) {
-    if (breed !== 'choose a dog breed') {
-            const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
-            const data = await response.json()
-            createGallery(data.message)
-        }
+  if (breed !== "choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await response.json();
+    //calling createGallery function and passing the fectched data as an argument
+    createGallery(data.message);
+  }
 }
 
 function createGallery(breedsImages) {
-    let currentPosition = 0
-    clearInterval(timer)
-    clearTimeout(deleteFirstPhotoDelay)
+  let currentPosition = 0;
 
-    let slideShow = document.getElementById('slideshow')
-    if (breedsImages.length > 1) {
-            slideShow.innerHTML = `
+  // Clear time out and interval for every item selected.
+  clearInterval(timer);
+  clearTimeout(deleteFirstPhotoDelay);
+
+  // selecting the slideshow div
+  let slideShow = document.getElementById("slideshow");
+
+  // check if there's only one image per breed, two, or more.. and set the currentPosition value for every situation
+  if (breedsImages.length > 1) {
+    slideShow.innerHTML = `
         <div class="slide" style="background-image: url('${breedsImages[0]}');"></div>
         <div class="slide" style="background-image: url('${breedsImages[1]}');"></div>
-    `
-    currentPosition += 2
-    if(breedsImages.length == 2) currentPosition += 0
+    `;
+    // currentPosition = currentPosition + 2
+    currentPosition += 2;
 
-    timer = setInterval(nextSlide, 3000)
+    // in case there's exactly two images per breed
+    if (breedsImages.length == 2) {
+      // currentPosition = currentPosition + 0
+      currentPosition += 0;
+    }
 
-    } else {
-            slideShow.innerHTML = `
+    //run the nextSlide function
+    timer = setInterval(nextSlide, 3000);
+  } else {
+    // in case there's only one image for a dog breed
+    slideShow.innerHTML = `
         <div class="slide" style="background-image: url('${breedsImages[0]}');"></div>
         <div class="slide"></div>
-    `
-    }
+    `;
+    //     .slide:nth-last-child(2) {
+    //     opacity: 1;
+    // }
+  }
 
-    function nextSlide() {
-        slideShow.insertAdjacentHTML("beforeend", `
+  function nextSlide() {
+    slideShow.insertAdjacentHTML(
+      "beforeend",
+      `
             <div class="slide" style="background-image: url('${breedsImages[currentPosition]}');"></div>
-        `)
-        deleteFirstPhotoDelay = setTimeout(function() {
-            document.querySelector('.slide').remove()
-        }, 1000);
-        if (currentPosition + 1 >= breedsImages.length) {
-            currentPosition = 0
-        } else {
-            currentPosition++
-        }
+        `
+    );
+
+    deleteFirstPhotoDelay = setTimeout(function () {
+      // automatically js select the first element that contain ".slide" class
+      document.querySelector(".slide").remove();
+    }, 1000);
+
+    // incrementing currentPosition value
+    if (currentPosition + 1 >= breedsImages.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
     }
+  }
 }
